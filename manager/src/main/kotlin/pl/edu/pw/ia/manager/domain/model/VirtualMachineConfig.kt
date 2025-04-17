a946ed3d18ed8b24c40047723768507553c78d48
Bartłomiej Krawczyk <01158771@pw.edu.pl>
2025-04-17 20:44:42 +0200

    Add type to config

--- manager/src/main/kotlin/pl/edu/pw/ia/manager/domain/model/VirtualMachineConfig.kt
+++ manager/src/main/kotlin/pl/edu/pw/ia/manager/domain/model/VirtualMachineConfig.kt
@@ -3,18 +3,23 @@ package pl.edu.pw.ia.manager.domain.model
 sealed interface VirtualMachineConfig {
     val name: VirtualMachineName
     val address: Address
+    val type: VirtualMachineType
 }
 
 data class Stateless(
     override val name: VirtualMachineName,
     override val address: Address,
-) : VirtualMachineConfig
+) : VirtualMachineConfig {
+    override val type: VirtualMachineType = VirtualMachineType.STATELESS
+}
 
 class LoadBalancer(
     override val name: VirtualMachineName,
     override val address: Address,
     val workers: Collection<Address>,
-) : VirtualMachineConfig
+) : VirtualMachineConfig {
+    override val type: VirtualMachineType = VirtualMachineType.LOAD_BALANCER
+}
 
 enum class VirtualMachineType {
     STATELESS,
@@ -23,4 +28,4 @@ enum class VirtualMachineType {
 }
 
 @JvmInline
-value class VirtualMachineName(val name: String)
+value class VirtualMachineName(val value: String)
