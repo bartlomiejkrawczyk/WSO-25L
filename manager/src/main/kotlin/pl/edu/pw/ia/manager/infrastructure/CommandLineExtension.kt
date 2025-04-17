145dbc21d7007b7966d9864b0f44fc9171607a55
Bartłomiej Krawczyk <01158771@pw.edu.pl>
2025-04-17 20:44:24 +0200

    Log cli exceptions

--- manager/src/main/kotlin/pl/edu/pw/ia/manager/infrastructure/CommandLineExtension.kt
+++ manager/src/main/kotlin/pl/edu/pw/ia/manager/infrastructure/CommandLineExtension.kt
@@ -1,7 +1,12 @@
 package pl.edu.pw.ia.manager.infrastructure
 
+import pl.edu.pw.ia.heartbeat.infrastructure.getLogger
 import java.io.File
 import java.io.InputStream
+
+class CommandLineExtension
+
+private val logger = getLogger(CommandLineExtension::class.java)
 
 data class IoResult(
     val stdOut: String,
@@ -12,15 +17,17 @@ fun String.runCommand(
     workingDir: File = File("."),
     environment: Map<String, String>? = null,
     input: InputStream? = null,
+    checked: Boolean = true,
 ): Result<IoResult> {
     val command = "\\s".toRegex().split(this)
-    return command.runCommand(workingDir, environment, input)
+    return command.runCommand(workingDir, environment, input, checked)
 }
 
 fun List<String>.runCommand(
     workingDir: File = File("."),
     environment: Map<String, String>? = null,
     input: InputStream? = null,
+    checked: Boolean = true,
 ): Result<IoResult> = runCatching {
     val builder = ProcessBuilder(this)
         .directory(workingDir)
@@ -48,3 +55,9 @@ fun List<String>.runCommand(
         stdErr = stdErrReader.readText(),
     )
 }
+    .onFailure { exc ->
+        logger.error("Execution for command: ${this.joinToString(separator = " ")} failed", exc)
+        if (checked) {
+            throw exc
+        }
+    }
