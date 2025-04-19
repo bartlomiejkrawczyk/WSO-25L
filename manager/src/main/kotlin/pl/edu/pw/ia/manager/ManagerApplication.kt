2051390edd5a9219fb4e144c148c363d815d6c47
Bartłomiej Krawczyk <01158771@pw.edu.pl>
2025-04-19 13:35:45 +0200

    Configure application properties

--- manager/src/main/kotlin/pl/edu/pw/ia/manager/ManagerApplication.kt
+++ manager/src/main/kotlin/pl/edu/pw/ia/manager/ManagerApplication.kt
@@ -1,11 +1,13 @@
 package pl.edu.pw.ia.manager
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 import org.springframework.web.reactive.config.EnableWebFlux
 
 @EnableWebFlux
 @SpringBootApplication
+@EnableConfigurationProperties
 class ManagerApplication
 
 fun main(args: Array<String>) {
---
+++ manager/src/main/kotlin/pl/edu/pw/ia/manager/infrastructure/ApplicationConfiguration.kt
@@ -0,0 +1,12 @@
+package pl.edu.pw.ia.manager.infrastructure
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.context.annotation.Configuration
+import pl.edu.pw.ia.heartbeat.domain.model.Address
+
+@Configuration
+@ConfigurationProperties(prefix = "application")
+data class ApplicationConfiguration(
+    var managers: Collection<Address> = emptyList(),
+    var publicIp: Address? = null,
+)
