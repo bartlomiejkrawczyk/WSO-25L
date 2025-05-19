94751eea5d6d8bf7bcc18daef85bca9565825122
Bartłomiej Krawczyk <01158771@pw.edu.pl>
2025-05-19 20:53:09 +0200

    Remove address from create machine request

--- manager/src/main/kotlin/pl/edu/pw/ia/manager/domain/model/CreateMachine.kt
+++ manager/src/main/kotlin/pl/edu/pw/ia/manager/domain/model/CreateMachine.kt
@@ -1,8 +1,5 @@
 package pl.edu.pw.ia.manager.domain.model
 
-import pl.edu.pw.ia.heartbeat.domain.model.Address
-
 data class CreateMachine(
     val name: VirtualMachineName,
-    val address: Address,
 )
